49096e414ce7d5ea44365df2e3829ff67d6da572
Gorkem Sever <gsever@k2m14s08.42kocaeli.com.tr>
2023-08-18 20:18:33 +0300

    Added some notes.

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,12 +6,26 @@
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 19:08:12 by gsever            #+#    #+#             */
-/*   Updated: 2023/07/21 23:01:19 by gsever           ###   ########.fr       */
+/*   Updated: 2023/08/18 17:18:34 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/IRCServ.hpp"
 
+/**
+ * @brief 
+ * 
+ * @TODO: Server olusturulduktan sonra client'ten gelen baglanti aninda;
+ * Server; baglanan client'in 
+ * 'file descriptor',
+ * 'address' ve 
+ * 'port'unu printle. Bunlari --DEBUG=true flagiyle calistirirken printlesin.
+ * ornek: ./ircserv --DEBUG=true <port> <password>
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int	main( int argc, char **argv )
 {
 	std::cout << "IRC Server started!" << std::flush << std::endl;
