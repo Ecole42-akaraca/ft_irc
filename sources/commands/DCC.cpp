5ccd88204db883b063d077869338418db5863865
Görkem Sever <gorkem_se_98@hotmail.com>
2023-09-03 18:10:33 +0300

    Color.hpp interesting name bug.

--- sources/commands/DCC.cpp
+++ sources/commands/DCC.cpp
@@ -6,6 +6,10 @@
  * @date 2023-09-03
  * 
  * @link: https://github.com/irssi/irssi/tree/master/src/irc/dcc
+ * @note: DCC (Direct Client-to-Client) mean; Send file with IRC.
+ * 
+ * @link: https://en.wikipedia.org/wiki/Reserved_IP_addresses
+ * @link: http://www.iana.org/numbers
  * 
  * @copyright Copyright (c) 2023
  * 
