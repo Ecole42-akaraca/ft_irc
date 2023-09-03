29ba1ca82ab0e624df3e95057140f5fe7932293e
Görkem Sever <gorkem_se_98@hotmail.com>
2023-09-03 18:45:51 +0300

    DCC protocol: Added some comment lines.

--- sources/commands/DCC.cpp
+++ sources/commands/DCC.cpp
@@ -16,3 +16,36 @@
  */
 #include "../../includes/Server.hpp"
 
+/**
+ * @brief 
+ * 
+ * @note;
+ * IRC (Internet Relay Chat) DCC (Direct Client-to-Client) komutları,
+	kullanıcıların birbirleriyle doğrudan bağlantı kurmasını sağlar.
+
+1.	**DCC CHAT {nick}**: Bu komut, belirtilen takma adla bir sohbet oturumu başlatır.
+Örneğin, "DCC CHAT john" komutu, "john" adlı kullanıcıyla bir sohbet oturumu başlatır.
+Bu hizmet, kullanıcıların bir DCC bağlantısı üzerinden birbirleriyle sohbet etmelerini sağlar.
+Trafik doğrudan kullanıcılar arasında gider ve IRC ağı üzerinden gitmez.
+DCC CHAT genellikle bir CTCP (Client-To-Client Protocol) HANSHAKE(el sıkışması) kullanılarak başlatılır.
+
+2.	**DCC SEND {nick} {dosya}**: Bu komut, belirtilen takma ada belirtilen dosyayı gönderir.
+Örneğin, "DCC SEND john image.jpg" komutu, "john" adlı kullanıcıya "image.jpg" dosyasını gönderir.
+
+3.	**DCC GET {nick} {dosya}**: Bu komut, belirtilen takma addan belirtilen dosyayı alır.
+Örneğin, "DCC GET john image.jpg" komutu, "john" adlı kullanıcıdan "image.jpg" dosyasını alır.
+
+4.	**DCC RESUME {nick} {dosya} {port} {position}**: Bu komut, belirtilen takma adla belirtilen 
+ dosyanın belirtilen konumdan indirilmesini sürdürür.
+Örneğin, "DCC RESUME john image.jpg 5000 1024" komutu, "john" adlı kullanıcıdan "image.jpg"
+ dosyasının 1024. bayttan itibaren indirilmesini sürdürür.
+
+5.	**DCC ACCEPT {nick} {dosya} {port} {position}**: Bu komut, belirtilen takma adla belirtilen
+ dosyanın belirtilen konumdan indirilmesini kabul eder.
+Örneğin, "DCC ACCEPT john image.jpg 5000 1024" komutu, "john" adlı kullanıcıdan "image.jpg"
+ dosyasının 1024. bayttan itibaren indirilmesini kabul eder.
+
+Bu komutlar genellikle IRC istemcilerinde bulunur ve doğrudan IRC sunucusu yerine
+ kullanıcılar arasında bağlantı kurarlar.
+ * 
+ */
